1eeeabbb643fe004ba3c58855cc8aadb2b67919d
Daniel.acf <daniel.albinocf@gmail.com>
2025-02-19 16:55:29 -0300

    process readjustment

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,8 +1,5 @@
-import "dotenv/config";
 import axios from "axios";
 
-// const { NODE_ENV } = process.env;
-// const URL = "http://localhost:3000/api";
 const URL = "https://spotify-replica-dan.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
